4c2c516ed1b0af0fec9e051ec0537ea2be06d444
dbvu99 <duc@bluespacecreative.com>
2022-07-30 10:54:52 -0500

    demo of depency

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from "./logo.svg"
 import "./App.css"
 import MyComponent from "./MyComponent"
+import UseEffect2Component from "./UseEffect2Component"
 
 function App() {
-  return <MyComponent />
+  return <UseEffect2Component />
 }
 
 export default App
---
+++ src/UseEffect2Component.js
@@ -0,0 +1,42 @@
+import React from "react"
+import { useState } from "react"
+import { useEffect } from "react"
+
+function UseEffect2Component() {
+  const [data, setData] = useState([])
+  const [loadingData, setLoadingData] = useState(true)
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    console.log("hey I am running")
+    setTimeout(() => {
+      const fakeData = [
+        { id: 1, name: "John" },
+        { id: 2, name: "Jane" },
+      ]
+
+      setData(fakeData)
+      setLoadingData(false)
+    }, 2000)
+  }, [count])
+
+  const increaseCountByOne = () => {
+    setCount(count + 1)
+  }
+
+  return (
+    <div>
+      <h1>My Data</h1>
+      <ul>
+        {loadingData && <li>Loading data...</li>}
+        {data.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+      <h1>My Count {count}</h1>
+      <button onClick={increaseCountByOne}>increate the count by 1</button>
+    </div>
+  )
+}
+
+export default UseEffect2Component
