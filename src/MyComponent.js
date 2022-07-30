1ba81caf366a97f10cb866b3ea1f9a0837ee99e3
dbvu99 <duc@bluespacecreative.com>
2022-07-30 10:40:54 -0500

    my very first component

---
+++ src/MyComponent.js
@@ -0,0 +1,35 @@
+// create a react functional component called MyComponent
+import { useState } from "react"
+
+import { useEffect } from "react"
+
+const MyComponent = () => {
+  const [data, setData] = useState([])
+  const [loadingData, setLoadingData] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      const fakeData = [
+        { id: 1, name: "John" },
+        { id: 2, name: "Jane" },
+      ]
+
+      setData(fakeData)
+      setLoadingData(false)
+    }, 2000)
+  }, [])
+
+  return (
+    <div>
+      <h1>My Data</h1>
+      <ul>
+        {loadingData && <li>Loading data...</li>}
+        {data.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default MyComponent
