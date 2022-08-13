abef4e865ec63ab3db64c5f722396ab128b56263
dbvu99 <duc@bluespacecreative.com>
2022-08-13 10:50:17 -0500

    implementing the filtering logic

--- src/fetch-api-demos/FetchApi2ndDemo.js
+++ src/fetch-api-demos/FetchApi2ndDemo.js
@@ -8,6 +8,8 @@ function FetchApi2ndDemo() {
   const [loadingData, setLoadingData] = useState(true)
 
   useEffect(() => {
+    // we do the initial data fetching inside this use effect
+
     //2. use fetch api to fetch data from the api
     fetch("https://dummyjson.com/products")
       .then((res) => {
@@ -27,13 +29,58 @@ function FetchApi2ndDemo() {
       .catch()
   }, [])
 
+  //
+
+  const [filterValue, setFilterValue] = useState("")
+  const [filteredData, setFilteredData] = useState(null)
+
+  useEffect(() => {
+    // we do the data filter inside this use effect
+
+    // [].filter()
+
+    if (filterValue) {
+      const filteredProducts = data.products.filter((item) => {
+        return item.title.includes(filterValue)
+      })
+
+      setFilteredData(filteredProducts)
+    } else {
+      setFilteredData(null)
+    }
+
+    // console.log("filer value is being changed")
+  }, [filterValue, data])
+
+  const handleFilterChange = (e) => {
+    setFilterValue(e.target.value)
+  }
+
   return (
-    <div>
+    <div className="container">
+      <form className="fixed-top w-100 bg-primary p-2">
+        <label className="me-3">Filter by title</label>
+        <input value={filterValue} onChange={handleFilterChange}></input>
+      </form>
+      <hr />
       {loadingData && <h1>Loading data...</h1>}
+      {filteredData?.length > 0 && (
+        <div className="pt-5">
+          <h1>Filtered Products</h1>
+          <div>Number of filtered products: {filteredData.length}</div>
+          <ul className="row list-unstyled">
+            {filteredData.map((product) => (
+              <ProductCard key={product.id} {...product} />
+            ))}
+          </ul>
+        </div>
+      )}
+      <hr />
+      <h1>All Products</h1>
       {data && (
-        <div>
+        <div className="pt-5">
           <div>Number of products: {data.total}</div>
-          <ul>
+          <ul className="row list-unstyled">
             {data.products.map((product) => (
               <ProductCard key={product.id} {...product} />
             ))}
--- src/fetch-api-demos/ProductCard.js
+++ src/fetch-api-demos/ProductCard.js
@@ -2,11 +2,11 @@ import React from "react"
 
 function ProductCard({ id, title, description, price, thumbnail, rating, ...theRestOfTheProps }) {
   return (
-    <li key={id}>
+    <li key={id} className="col-12 col-sm-6 col-md-4 px-3">
       <h2>{title}</h2>
       <p>{description}</p>
       <div>rating: {rating}</div>
-      <img src={thumbnail} alt="" />
+      <img className="w-100" src={thumbnail} alt="" />
       {/* {JSON.stringify(theRestOfTheProps)} */}
     </li>
   )
